39d27d17dca0a19f1a321894604b18b9a1deaf3e
Marc-André Martin <marcandre@mamarmite.com>
2022-04-14 02:28:48 -0400

    basic change and test to make the up and down chain.

--- src/GameEngine/core.js
+++ src/GameEngine/core.js
@@ -20,6 +20,11 @@ export default class GameEngine {
     groups;
 
     hero;
+    chain;
+    chainComposite;
+    chainMainConstraint;
+    chainMaxLenght;
+    chainMinLenght;
 
     constructor(w=800, h=600) {
         this.stageWidth = w;
@@ -28,7 +33,8 @@ export default class GameEngine {
             width: this.stageWidth,
             height: this.stageHeight,
         };
-
+        this.chainMinLenght = 1;
+        this.chainMaxLenght = 200;
         this.debugRenderOptions = {
             showAngleIndicator: true,
             //showCollisions: true,
@@ -58,7 +64,6 @@ export default class GameEngine {
          this.entities();
 
          this.controls();
-         this.mouse();
     }
 
     render() {
@@ -79,10 +84,13 @@ export default class GameEngine {
 
     entities() {
         var group = Matter.Body.nextGroup(true);
+
         let moveableStack01 = new MoveableStack({});
+        let floor = Matter.Bodies.rectangle(this.stageWidth/2, this.stageHeight-10, this.stageWidth, 10, { isStatic: true });
+
         Matter.Composite.add(this.world, [
             moveableStack01,
-            Matter.Bodies.rectangle(400, 600, 800, 50, { isStatic: true }),
+            floor
         ]);
 
         var startX = 200, startY = 50, lastX = startX, lastY = startY,
@@ -92,35 +100,28 @@ export default class GameEngine {
         this.hero = Matter.Bodies.circle(startX, startY, 30, { collisionFilter: { group: group }, chamfer: 0 });
         this.hero.isStatic = true;
 
-        var chainHand = Matter.Bodies.circle(startX, startY, 30, { collisionFilter: { group: group }, chamfer: 0 });
+        var chainHand = Matter.Bodies.circle(startX, startY, 10, { collisionFilter: { group: group }, chamfer: 0, density: 0.1 });
 
-        var craneChain = Matter.Composites.stack(startX+15, startY+15, 15, 1, 1, 1, function(x, y) {
+        this.chain = Matter.Composites.stack(startX+15, startY+15, 15, 1, 1, 1, function(x, y) {
             lastX = x; lastY = y;
-            return Matter.Bodies.rectangle(x - 30, y -30, chainLinkHeight, chainLinkWidth, { collisionFilter: { group: group }, chamfer: 0.1, stiffness: 1 });
+            return Matter.Bodies.rectangle(x - 30, y -30, chainLinkHeight, chainLinkWidth, { collisionFilter: { group: group }, chamfer: 0.1, density: 0.1 });
         });
 
 
-        var chainAnchor = craneChain.bodies[0];
-        craneChain.bodies.push(chainHand);
+        var chainAnchor = this.chain.bodies[0];
+        this.chain.bodies.push(chainHand);
 
-        var chainComposite = Matter.Composites.chain(craneChain, 0.4, 0, -0.4, 0, { stiffness: 1, length: 0 });
-        var chainAnchorConstraint = Matter.Constraint.create({
+        this.chainComposite = Matter.Composites.chain(this.chain , 0.4, 0, -0.4, 0, { length: 0.001 });
+        this.chainMainConstraint = Matter.Constraint.create({
             bodyA: this.hero,
-            bodyB: chainAnchor,
-            stiffness: 1,
-            length: 0,
+            bodyB: chainAnchor
         });
-
-        group = Matter.Body.nextGroup(true);
 
         Matter.Composite.add(this.world, [
             this.hero,
-            craneChain,
-            chainAnchorConstraint,
-            Matter.Bodies.rectangle(400, 600, 1200, 50.5, { isStatic: true })
+            this.chain,
+            this.chainMainConstraint
         ]);
-
-        var group = Matter.Body.nextGroup(true);
     }
 
     controls() {
@@ -138,11 +139,18 @@ export default class GameEngine {
 
             },
             KeyW: () => {
-                Matter.Body.translate(this.hero, {x:0, y: -muscular});
-
+                //Matter.Body.translate(this.hero, {x:0, y: -muscular});
+                this.chain.bodies.forEach(function(body) {
+                    body.density += 0.001;
+                    console.log(body);
+                });
             },
             KeyS: () => {
-                Matter.Body.translate(this.hero, {x:0, y:muscular});
+                //Matter.Body.translate(this.hero, {x:0, y: +muscular});
+                this.chain.bodies.forEach(function(body) {
+                    body.density -= 0.001;
+                    console.log(body);
+                });
 
             },
             KeyD: () => {
@@ -165,21 +173,23 @@ export default class GameEngine {
                 keyHandlers[k]?.();
             });
         });
+
+        this.mouse();
     }
 
     mouse() {
-        var mouse = Matter.Mouse.create(render.canvas),
-            mouseConstraint = Matter.MouseConstraint.create(engine, {
+        var mouse = Matter.Mouse.create(this.renderer.canvas),
+            mouseConstraint = Matter.MouseConstraint.create(this.engine, {
                 mouse: mouse,
                 constraint: {
                     stiffness: 0.1,
                     render: {
-                        visible: false
+                        visible: true
                     }
                 }
             });
 
-        Matter.Composite.add(world, mouseConstraint);
+        Matter.Composite.add(this.world, mouseConstraint);
 
         // keep the mouse in sync with rendering
         this.renderer.mouse = Matter.mouse;
--- src/engine.js
+++ src/engine.js
@@ -1,4 +1,4 @@
 import GameEngine from "./GameEngine/core.js";
 
-const game = new GameEngine();
+const game = new GameEngine(1920, 1080);
 game.init();
